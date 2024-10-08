bd4527886451078543559b97a5a4e08f84de68d7
knockknock <yangban11111@gmail.com>
2024-10-08 11:14:45 +0900

    Initial Commit

---
+++ PbxConflict/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PbxConflict
+//
+//  Created by 양승혜 on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PbxConflict/PbxConflictApp.swift
@@ -0,0 +1,17 @@
+//
+//  PbxConflictApp.swift
+//  PbxConflict
+//
+//  Created by 양승혜 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PbxConflictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
